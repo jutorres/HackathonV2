94ce0d97d4cfcc1ec6739a29ce769b7cfdfaec32
jutorres <torresjujuliana@gmail.com>
2016-04-13 12:20:53 -0300

    Class funcionario created

--- src/com/stefanini/hackathon2/entidades/Funcionario.java
+++ src/com/stefanini/hackathon2/entidades/Funcionario.java
@@ -1,5 +1,97 @@
 package com.stefanini.hackathon2.entidades;
 
-public class Funcionario {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Funcionario {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idFuncionario;
+	@Column(nullable=false)
+	private String nome;
+	@Column(nullable=false)
+	private Double salario;
+	@Column(nullable=false)
+	private String email;
+	
+	public Funcionario() {
+	}
+
+	public Integer getIdFuncionario() {
+		return idFuncionario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setIdFuncionario(Integer idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idFuncionario == null) ? 0 : idFuncionario.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((salario == null) ? 0 : salario.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+
+	@Override
+	public final boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (idFuncionario == null) {
+			if (other.idFuncionario != null)
+				return false;
+		} else if (!idFuncionario.equals(other.idFuncionario))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (salario == null) {
+			if (other.salario != null)
+				return false;
+		} else if (!salario.equals(other.salario))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		return true;
+	}
+	
 }
